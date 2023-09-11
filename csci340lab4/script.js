5758b5bb2a67ef32dbd53673ef3054a56c0aa056
Colten Berry <berrycj@hendrix.edu>
2023-09-11 14:52:59 -0500

    blog

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $('p').attr("color", "red")
+    $("p").attr("color", "red");
+    console.log("load");
+    $("#cont2").attr("color", "red")
+    $("#button1").click(function() {
+        $.ajax({
+            dataType: "json",
+            url: "/swagger/v1/swagger.json",
+            success: function(results) {
+                console.log(results[title]);
+                $('p').text(results[title])
+            }
+        })
+    })
 })
