bc2ebfa0c9d00dea6fdba4597029082b0e6b690b
Colten Berry <berrycj@hendrix.edu>
2023-09-12 20:30:16 -0500

    Lab 4 Finished

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -5,10 +5,29 @@ $(document).ready(function() {
     $("#button1").click(function() {
         $.ajax({
             dataType: "json",
-            url: "/swagger/v1/swagger.json",
+            url: "https://cataas.com/cat?json=true",
             success: function(results) {
-                console.log(results[title]);
-                $('p').text(results[title])
+                $('#cat').attr("src", "https://cataas.com/" + results["url"])
+                .attr("height", "200px");
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+        })
+    })
+    $("#button2").click(function() {
+        console.log("click2");
+        $.ajax({
+            dataType: "json",
+            url: "https://api.chucknorris.io/jokes/random",
+            success: function(results) {
+                console.log("success");
+                $('#button-2-content').text(results["value"]);
+                console.log(results["value"]);
+            },
+            error: function(xhr, status, error) {
+                console.log("failure");
+                console.log(results[value]);
             }
         })
     })
